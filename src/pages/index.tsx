dcbc3386b193519ccabf4464849746ce62d0fd17
Andrew Quenehen <andrewqsantos@protonmail.com>
2021-12-03 10:15:09 -0300

    header basic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
+import Head from 'next/head';
 import styles from '../styles/styles.module.scss';
 
 export default function Home() {
   return (
+    <>
+    <Head>
+      <title>Board - Organizando suas tarefas.</title>
+    </Head>
     <div>
       <h1>Primeiro projeto com NextJS</h1>
     </div>
+    </>
   )
 }
